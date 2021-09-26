70c66147caa7ddf6be60d4b36a06f7b7cfcada88
JungHyoMoon <dsinhome70@gmail.com>
2021-09-26 22:44:47 +0900

    prisma is awsomly easy. todo: prisma studio?

---
+++ prisma/migrations/20210926120311_fix/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Changed the type of `year` on the `Movie` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "Movie" DROP COLUMN "year",
+ADD COLUMN     "year" INTEGER NOT NULL,
+ALTER COLUMN "title" SET DATA TYPE TEXT;
--- server.js
+++ server.js
@@ -1,36 +1,68 @@
 import { ApolloServer, gql } from "apollo-server";
+import { PrismaClient } from "@prisma/client";
 // 아폴로의 개짓거리를 막고, playground로 직행하게 해주는 플러그인.
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
+// client: 데이터베이스와 어떻게 대화하는가
+const client = new PrismaClient();
+
+// grqphql type들의 정의
 const typeDefs = gql`
     type Movie {
-        id: Int
-        title: String
-        year: Int
+        id: Int!
+        year: Int!
+        title: String!
+        genre: String
+        createdAt: String!
+        updatedAt: String!
     }
 
     type Query {
         movies: [Movie]
-        movie: Movie
+        movie(id: Int!): Movie
     }
 
     type Mutation {
-        createMovie(title: String!): Boolean
-        deleteMovie(title: String!): Boolean
+        createMovie(title: String!, year: Int!, genre: String): Movie
+        deleteMovie(id: Int!): Movie
+        updateMovie(id: Int!, title: String, year: Int, genre: String): Movie
     }
 `;
 
+// 요청을 '해결' 하기 위한 resolver들.
 const resolvers = {
+    // Query == GET
     Query: {
-        movies: () => [],
-        movie: () => ({ title: "hello", year: 2021 }),
+        movies: () => client.movie.findMany(),
+        movie: (_, { id }) =>
+            client.movie.findUnique({
+                where: {
+                    id,
+                },
+            }),
     },
+    // Mutation == POST, PUT, DELETE
     Mutation: {
-        createMovie: (_, { title }) => {
-            console.log(title);
-            return true;
-        },
-        deleteMovie: () => "",
+        createMovie: (_, { title, year, genre }) =>
+            client.movie.create({
+                data: {
+                    title,
+                    year,
+                    genre,
+                },
+            }),
+        deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
+        updateMovie: (_, { id, title, year, genre }) =>
+            client.movie.update({
+                where: {
+                    id,
+                },
+                data: {
+                    title,
+                    year,
+                    genre,
+                },
+            }),
     },
 };
 
