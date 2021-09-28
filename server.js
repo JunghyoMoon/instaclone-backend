2769f8fac3831eac2c3ccf6d2ddf08c39d901cba
JungHyoMoon <dsinhome70@gmail.com>
2021-09-28 11:38:04 +0900

    dotenv is done. todo: proceed to next chapter

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+// dotenv는 최우선적으로 불러와야 함.
 import { ApolloServer } from "apollo-server";
 // 아폴로의 개짓거리를 막고, playground로 직행하게 해주는 플러그인.
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
@@ -8,8 +10,10 @@ const server = new ApolloServer({
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
+const PORT = process.env.PORT;
+
 server
-    .listen()
+    .listen(PORT)
     .then(() =>
-        console.log("Your server is running on http://localhost:4000/ 😎")
+        console.log(`Your server is running on http://localhost:${PORT}/ 🚀`)
     );
