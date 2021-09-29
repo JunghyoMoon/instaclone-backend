bbd31bfdd97f9e5edf9f6f706f74d713f9502568
JungHyoMoon <dsinhome70@gmail.com>
2021-09-29 13:38:42 +0900

    todo: existing user 처리

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import client from "../client";
 
 export default {
@@ -19,9 +20,18 @@ export default {
                     ],
                 },
             });
-            console.log(exictingUser);
-            // if not : hash password,
-            //          then, save and return user.
+            // if not : hash(난독화?) password, (이미 해쉬된 문자열은 다시 복원 불가.)
+            const uglyPassword = await bcrypt.hash(password, 10);
+            // then, save and return user.
+            return client.user.create({
+                data: {
+                    username,
+                    email,
+                    firstName,
+                    lastName,
+                    password: uglyPassword,
+                },
+            });
         },
     },
 };
