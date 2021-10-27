2a1ba47a7ea939a39b186bc455c4b2a1c5d98601
JungHyoMoon <dsinhome70@gmail.com>
2021-10-27 15:30:13 +0900

    disconnect hashtag is done

--- photos/editPhoto/editPhoto.resolvers.js
+++ photos/editPhoto/editPhoto.resolvers.js
@@ -7,19 +7,34 @@ export default {
             async (_, { id, caption }, { loggedInUser }) => {
                 // findUnique: 'unique'한 요소로만 찾을 수 있음?
                 // findFirst: 기준에 맞는 첫번째 요소 반환
-                const ok = await client.photo.findFirst({
+                const oldPhoto = await client.photo.findFirst({
                     where: {
                         id,
                         userId: loggedInUser.id,
                     },
+                    include: {
+                        hashtags: true,
+                    },
                 });
 
-                if (!ok) {
+                if (!oldPhoto) {
                     return {
                         ok: false,
                         error: "photo not found (or, this is not your photo.)",
                     };
                 }
+
+                const photo = await client.photo.update({
+                    where: { id },
+                    data: {
+                        caption,
+                        hashtags: {
+                            // 기존에 있던 hashtag들과의 연결 해제(삭제)
+                            disconnect: oldPhoto.hashtags,
+                            // todo: generate new hashtags
+                        },
+                    },
+                });
             }
         ),
     },
